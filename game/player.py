f8a94d0368c0f767df7bf4ca7be0241f661a97df
FernandoWeigandt <f.weigandt@alumno.um.edu.ar>
2023-08-25 14:13:01 -0300

    Player and Board class and tests done

--- game/player.py
+++ game/player.py
@@ -1,7 +1,8 @@
 
-
 class Player:
-    def __init__(self):
+    def __init__(self,name,score,):
+        self.name=name
+        self.score=score
         self.tiles=[]
 
 
--- tests/test_player.py
+++ tests/test_player.py
@@ -3,4 +3,8 @@ from game.player import *
 
 
 class TestPlayer(unittest.TestCase):
-    pass
+    def test_init(self):
+        player=Player('Fernando',0)
+        self.assertEqual(player.name,'Fernando')
+        self.assertEqual(player.score,0)
+        self.assertEqual(player.tiles,[])
