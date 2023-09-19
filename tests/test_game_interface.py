65607b0b0f7d2ef926571e943aa03a134b078948
FernandoWeigandt <f.weigandt@alumno.um.edu.ar>
2023-09-19 20:02:42 -0300

    Trying to understand mock

--- tests/test_game_interface.py
+++ tests/test_game_interface.py
@@ -1,15 +1,19 @@
-import unittest
-from unittest.mock import patch
-from game.cli.game_interface import main
+# import unittest
+# from unittest.mock import patch
+# from game.cli.game_interface import main
 
-class TestGameInterface(unittest.TestCase):
-    @patch('builtins.input', side_effect=['2'])
-    def test_main(self, mock_input):
-        main()
-        self.assertEqual(mock_input.call_count, 1)
-        self.assertEqual(mock_input.call_args, (('How many players? ',),))
-        self.assertEqual(mock_input.call_args_list, [(('How many players? ',),)])
-        self
-if __name__ == '__main__':
-    unittest.main()
-    
+# class TestGameInterface(unittest.TestCase):
+#     @patch('builtins.input', side_effect=['2'])
+#     def test_main(self, mock_input):
+#         main()
+#         self.assertEqual(mock_input.call_count, 1)
+#         self.assertEqual(mock_input.call_args, (('How many players? ',),))
+#         self.assertEqual(mock_input.call_args_list, [(('How many players? ',),)])
+
+
+
+
+
+# if __name__ == '__main__':
+#     unittest.main()
+
